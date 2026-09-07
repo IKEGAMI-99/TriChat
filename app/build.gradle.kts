ed5b043344405db850bcdba10efabec4f82595a4
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-08 07:01:10 +0900

    release: bump TriChat to v0.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ikegami99.trichat"
         minSdk = 33
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "0.1.8"
     }
 
     buildFeatures {
